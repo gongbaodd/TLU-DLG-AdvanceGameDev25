04e7454355d61845ed3eb26e5311e31a2ae89774
gongbaodd@ProArt <gongbaodd@qq.com>
2025-02-26 23:14:45 +0200

    feat: add Diablo scene and associated addressable asset configurations

--- Cat/Assets/Scenes/TestStarter/Controllers/StarterSceneController.cs
+++ Cat/Assets/Scenes/TestStarter/Controllers/StarterSceneController.cs
@@ -1,13 +1,12 @@
 using UnityEngine;
 using UnityEngine.AddressableAssets;
-using UnityEngine.ResourceManagement.AsyncOperations;
-using UnityEngine.ResourceManagement.ResourceProviders;
 using UnityEngine.SceneManagement;
 
 public class StarterSceneController : MonoBehaviour
 {
     [SerializeField] string gardenSceneAddress = "Assets/Scenes/Garden/Garden.unity";
     [SerializeField] string fruitNinjaSceneAddress = "Assets/Scenes/FruitNinja/FruitNinja.unity";
+    [SerializeField] string diabloSceneAddress = "Assets/Scenes/Diablo/Diablo.unity";
     public void LoadCardenScene()
     {
         Addressables.LoadSceneAsync(gardenSceneAddress, LoadSceneMode.Single);
@@ -15,5 +14,9 @@ public class StarterSceneController : MonoBehaviour
 
     public void LoadFruitNinjaScene() {
         Addressables.LoadSceneAsync(fruitNinjaSceneAddress, LoadSceneMode.Single);
+    }
+
+    public void LoadDiabloScene() {
+        Addressables.LoadSceneAsync(diabloSceneAddress, LoadSceneMode.Single);
     }
 }
