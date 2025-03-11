8ef0daeb908e8108746f7c2074a3133980ca58e0
gongbaodd@ProArt <gongbaodd@qq.com>
2025-03-11 13:07:36 +0200

    use AssetReference

--- Cat/Assets/Scenes/TestStarter/Controllers/StarterSceneController.cs
+++ Cat/Assets/Scenes/TestStarter/Controllers/StarterSceneController.cs
@@ -1,30 +1,33 @@
+using System;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.SceneManagement;
 
+
 public class StarterSceneController : MonoBehaviour
 {
-    [SerializeField] string gardenSceneAddress = "Assets/Scenes/Garden/Garden-design.unity";
-    [SerializeField] string fruitNinjaSceneAddress = "Assets/Scenes/FruitNinja/FruitNinja.unity";
-    [SerializeField] string diabloSceneAddress = "Assets/Scenes/Diablo/Diablo.unity";
-    [SerializeField] string inventorySceneAddress = "Assets/Scenes/Inventory/Inventory.unity";
+    [SerializeField] AssetReferenceT<SceneAsset> gardenScene;    
+    [SerializeField] AssetReferenceT<SceneAsset> fruitNinjaScene;
+    [SerializeField] AssetReferenceT<SceneAsset> diabloScene;
+    [SerializeField] AssetReferenceT<SceneAsset> inventoryScene;
     public void LoadGardenScene()
     {
-        Addressables.LoadSceneAsync(gardenSceneAddress, LoadSceneMode.Single);
+        gardenScene.LoadSceneAsync(LoadSceneMode.Single);
     }
 
     public void LoadFruitNinjaScene()
     {
-        Addressables.LoadSceneAsync(fruitNinjaSceneAddress, LoadSceneMode.Single);
+        fruitNinjaScene.LoadSceneAsync(LoadSceneMode.Single);
     }
 
     public void LoadDiabloScene()
     {
-        Addressables.LoadSceneAsync(diabloSceneAddress, LoadSceneMode.Single);
+        diabloScene.LoadSceneAsync(LoadSceneMode.Single);
     }
 
     public void LoadInventoryScene()
     {
-        Addressables.LoadSceneAsync(inventorySceneAddress, LoadSceneMode.Single);
+        inventoryScene.LoadSceneAsync(LoadSceneMode.Single);
     }
 }
