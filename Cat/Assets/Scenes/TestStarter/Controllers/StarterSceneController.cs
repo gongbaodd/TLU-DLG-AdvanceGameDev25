8acfca636b3e7a7bf3ac5c364201b300bbdd41f0
gongbaodd@ProArt <gongbaodd@qq.com>
2025-03-11 23:45:32 +0200

    release handler

--- Cat/Assets/Scenes/TestStarter/Controllers/StarterSceneController.cs
+++ Cat/Assets/Scenes/TestStarter/Controllers/StarterSceneController.cs
@@ -1,33 +1,42 @@
 using System;
+using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 using UnityEngine.SceneManagement;
 
 
 public class StarterSceneController : MonoBehaviour
 {
-    [SerializeField] AssetReference gardenScene;    
+    [SerializeField] AssetReference gardenScene;
     [SerializeField] AssetReference fruitNinjaScene;
     [SerializeField] AssetReference diabloScene;
     [SerializeField] AssetReference inventoryScene;
+    
+    private AsyncOperationHandle handle;
     public void LoadGardenScene()
     {
-        gardenScene.LoadSceneAsync(LoadSceneMode.Single);
+        handle = gardenScene.LoadSceneAsync(LoadSceneMode.Single);
     }
 
     public void LoadFruitNinjaScene()
     {
-        fruitNinjaScene.LoadSceneAsync(LoadSceneMode.Single);
+        handle = fruitNinjaScene.LoadSceneAsync(LoadSceneMode.Single);
     }
 
     public void LoadDiabloScene()
     {
-        diabloScene.LoadSceneAsync(LoadSceneMode.Single);
+        handle = diabloScene.LoadSceneAsync(LoadSceneMode.Single);
     }
 
     public void LoadInventoryScene()
     {
-        inventoryScene.LoadSceneAsync(LoadSceneMode.Single);
+        handle = inventoryScene.LoadSceneAsync(LoadSceneMode.Single);
+    }
+
+    void OnDestroy()
+    {
+        handle.Release();
     }
 }
