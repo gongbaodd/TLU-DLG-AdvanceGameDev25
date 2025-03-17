5133566cb2c3ba268cb6f311e2277298d2d1c3d5
gongbaodd@ProArt <gongbaodd@qq.com>
2025-03-17 19:20:17 +0200

    update fruitController with collision functions

--- Cat/Assets/Scenes/FruitNinja/Scripts/FruitController.cs
+++ Cat/Assets/Scenes/FruitNinja/Scripts/FruitController.cs
@@ -7,18 +7,46 @@ namespace Assets.Scenes.FruitNinja.Scripts
     {
         private Rigidbody rb;
 
+        private GameObject explosionFX;
         [SerializeField] private float speed = 15f;
         [SerializeField] private float torque = 0.1f;
+        [SerializeField] private float destroyDelay = 2f;
+        [SerializeField] private float destroyForce = 50f;
+
         void Start()
         {
             rb = GetComponent<Rigidbody>();
             rb.AddForce(Vector3.up * speed, ForceMode.Impulse);
             rb.AddTorque(RandomTorque() * torque, ForceMode.Impulse);
+
+            explosionFX = GameObject.FindGameObjectWithTag("ExplosionFX");
+
+            if (explosionFX == null)
+            {
+                Debug.LogError("ExplosionFX not found!");
+            }
         }
 
         private Vector3 RandomTorque()
         {
             return new Vector3(Random.Range(-1, 1), Random.Range(-1, 1), Random.Range(-1, 1));
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("CatPaw"))
+            {
+
+                Vector3 forceDirection = (transform.position - other.transform.position).normalized;
+                rb.AddForce(forceDirection * destroyForce, ForceMode.Impulse);
+
+                explosionFX.GetComponent<ParticleSystem>().Play();
+
+                rb.isKinematic = true;
+                rb.useGravity = false;
+                rb.detectCollisions = false;
+                Destroy(gameObject, destroyDelay);
+            }
         }
     }
 }
