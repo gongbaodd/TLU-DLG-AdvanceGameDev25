7d154b330919d514661cebd618b4b4b4dde13f9d
gongbaodd@ProArt <gongbaodd@qq.com>
2025-04-19 15:14:17 +0300

    change box

--- Cat/Assets/Scenes/Diablo/Scripts/BoxController.cs
+++ Cat/Assets/Scenes/Diablo/Scripts/BoxController.cs
@@ -5,25 +5,41 @@ namespace Assets.Scenes.Diablo.Scripts
     [RequireComponent(typeof(BoxCollider))]
     public class BoxController : MonoBehaviour
     {
-         public static readonly string BOXTAG = "DiabloBox";
+        public static readonly string BOXTAG = "DiabloBox";
         [SerializeField] BoxConfig boxConfig;
-        void Awake()
+
+        void OnFindMemory()
         {
-            
+            var manager = DiabloController.gameManager;
+
+            if (manager)
+            {
+                manager.GetComponent<DiabloController>().Win();
+            }
+        }
+
+        void OnTriggerMonster() {
+            print("run!");
         }
 
         void OnTriggerEnter(Collider other)
         {
             if (other.CompareTag("Player"))
             {
-                if (boxConfig.isMemoryStored)
+                switch (boxConfig.content)
                 {
-                    var manager = DiabloController.gameManager;
-
-                    if (manager)
-                    {
-                        manager.GetComponent<DiabloController>().Win();
-                    }
+                    case BoxContent.None:
+                        break;
+                    case BoxContent.Memory:
+                        OnFindMemory();
+                        break;
+                    case BoxContent.Health:
+                        break;
+                    case BoxContent.Monster:
+                        OnTriggerMonster();
+                        break;
+                    default:
+                        throw new System.Exception("Invalid box content");
                 }
             }
         }
