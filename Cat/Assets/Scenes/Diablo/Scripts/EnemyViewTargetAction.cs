6760335e15fe09bc9e904fb95696fb0428ef83c4
gongbaodd@ProArt <gongbaodd@qq.com>
2025-04-26 12:00:30 +0300

    add Detector logic

---
+++ Cat/Assets/Scenes/Diablo/Scripts/EnemyViewTargetAction.cs
@@ -0,0 +1,23 @@
+using Assets.Scenes.Diablo.Scripts;
+using System;
+using Unity.Behavior;
+using UnityEngine;
+using Action = Unity.Behavior.Action;
+using Unity.Properties;
+
+[Serializable, GeneratePropertyBag]
+[NodeDescription(name: "view target", story: "[Detector] finds the [target]", category: "Action", id: "34f99b21a3d5b9b70fedbb6c335fc7ea")]
+public partial class EnemyViewTargetAction : Action
+{
+    [SerializeReference] public BlackboardVariable<EnemyVisionDetectorController> Detector;
+    [SerializeReference] public BlackboardVariable<GameObject> Target;
+
+
+    protected override Status OnUpdate()
+    {
+        Target.Value = Detector.Value.GetTarget();
+        return Target.Value == null ? Status.Failure : Status.Success;
+    }
+
+}
+
--- Cat/Assets/Scenes/Diablo/Scripts/SkeletonController.cs
+++ Cat/Assets/Scenes/Diablo/Scripts/SkeletonController.cs
@@ -4,33 +4,10 @@ namespace Assets.Scenes.Diablo.Scripts
 {
     [RequireComponent(typeof(SkeletonAnimatorController))]
     [RequireComponent(typeof(CursorLabelController))]
+    [RequireComponent(typeof(EnemyVisionDetectorController))]
     public class SkeletonController : MonoBehaviour
     {
         public EnemyConfig config;
-
-        // void DetectPlayer()
-        // {
-        //     Collider[] hits = Physics.OverlapSphere(transform.position, config.detectionRange, LayerMask.GetMask("Player"));
-
-        //     Gizmos.color = Color.yellow;
-        //     // Gizmos.DrawWireSphere(transform.position, config.detectionRange);
-
-        //     foreach (Collider hit in hits)
-        //     {
-        //         Transform target = hit.transform;
-        //         Vector3 direction = (target.position - transform.position).normalized;
-
-        //         if (Vector3.Angle(transform.forward, direction) < config.viewAngle / 2)
-        //         {
-        //             print("player detected");
-        //         }
-        //     }
-        // }
-
-        void Update()
-        {
-            // DetectPlayer();
-        }
     }
 }
 
