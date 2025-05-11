aaa9c0483a0e192c60c6e1ecbfe8dba29dcedab0
gongbaodd@ProArt <gongbaodd@qq.com>
2025-05-11 14:49:41 +0300

    load scenes

---
+++ Cat/Assets/Scripts/SceneManagerController.cs
@@ -0,0 +1,42 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using System.Threading.Tasks;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerController : MonoBehaviour
+{
+    public static SceneManagerController Instance { get; private set; }
+    [SerializeField] GameObject loading;
+    void ToggleLoading(bool value) => loading.SetActive(value);
+
+    async Task LoadScene(AssetReference scene) {
+        ToggleLoading(true);
+        var handler = Addressables.LoadSceneAsync(scene, LoadSceneMode.Single);
+        await handler.Task;
+        ToggleLoading(false);
+    }
+    [SerializeField] AssetReference DebugScene;
+    public void GotoDebugScene()
+    {
+        _ = LoadScene(DebugScene);
+    }
+
+    void ToSingleton()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Awake()
+    {
+        ToSingleton();
+        ToggleLoading(false);
+    }
+}
