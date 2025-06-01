3c248f598be824b579275ca82476e1fe2e01e492
MartinWachira <67173409+mmwachira@users.noreply.github.com>
2025-06-01 12:45:32 +0300

    refactor: remove commented code

--- Cat/Assets/Scenes/Garden/Dialogue/Tutorial.ink
+++ Cat/Assets/Scenes/Garden/Dialogue/Tutorial.ink
@@ -1,19 +1,13 @@
 VAR speaker = "god"
 VAR name_given = 0
-VAR is_fruit_ninja_done = false
-VAR is_diablo_done = false
 VAR is_goto_fruit_ninja = false
-VAR is_goto_diablo = false
 
+(You, a small but determined cat, awaken in your home's backyard.)
 
-You, a small but determined cat, awaken in your home's backyard.
-
-+ [Continue] -> START
++[Continue]-> START
 
 === START ===
 ~ speaker = "god"
-{is_fruit_ninja_done && !is_diablo_done: -> TUTORIAL2}
-{is_fruit_ninja_done && is_diablo_done: -> ending}
 ???: So... you’ve come after me.
 
 * [Say nothing.] -> SILENT
@@ -53,11 +47,7 @@ GHOST KING: You want your owner back? Then chase after me. But you’ll have to 
     ???: In the realm ahead, objects will fly through the air. Slice them with your paws. Some hold memory pieces, others are just... fruit.
 }
  
-+[Continue]-> GOTO_FRUIT_NINJA
-
-=== GOTO_FRUIT_NINJA ===
-~ is_goto_fruit_ninja = true
--> DONE
++[Continue]-> TUTORIAL2
 
 === TUTORIAL2 ===
 { name_given > 0:
@@ -66,12 +56,8 @@ GHOST KING: You want your owner back? Then chase after me. But you’ll have to 
     ???: Once you've collected enough, the gate will open. That’s when your claws will truly be tested.
 }
 
-* [I’m ready.] -> GOTO_DIABLO
-* [You'll regret this!] -> GOTO_DIABLO
-
-=== GOTO_DIABLO ===
-~ is_goto_diablo = true
--> DONE
+* [I’m ready.] -> ending
+* [You'll regret this!] -> ending
 
 === ending ===
 ~ speaker = "god"
@@ -81,4 +67,9 @@ GHOST KING: You want your owner back? Then chase after me. But you’ll have to 
     ???: Then come, little flame. Let’s see how far your devotion burns...
 }
 
-+ [Continue] -> END
++[Continue] -> GOTO_FRUIT_NINJA
+
+=== GOTO_FRUIT_NINJA ===
+~ is_goto_fruit_ninja = true
+-> END
+
--- Cat/Assets/Scenes/Garden/Scripts/DialogController.cs
+++ Cat/Assets/Scenes/Garden/Scripts/DialogController.cs
@@ -5,6 +5,7 @@ using UnityEngine.UIElements;
 using System.Collections.Generic;
 using Button = UnityEngine.UIElements.Button;
 using UnityEngine.Events;
+using System.Linq;
 
 namespace Assets.Scenes.Garden.Scripts
 {
@@ -19,41 +20,36 @@ namespace Assets.Scenes.Garden.Scripts
         VisualElement contentContainer;
         readonly List<Button> choices = new();
 
-        enum Speaker { God, Boss }
+        enum Speaker { God }
         Speaker CurrentSpeaker
         {
             get
             {
-                string speaker = story.variablesState["speaker"] as string;
-
-                if (speaker == "god") return Speaker.God;
-                else return Speaker.Boss;
+                // Check if the speaker variable is set in the story
+                if (story.variablesState.Contains("speaker"))
+                {
+                    // If the speaker variable is present, use it
+                    string speaker = story.variablesState["speaker"] as string;
+                    if (speaker == "god") return Speaker.God;
+                }
+                return Speaker.God;
             }
         }
 
         string currentText = "";
 
-        [SerializeField] UnityEvent OnNextScene;
         bool IsGotoFruitNinja
         {
             get => story.variablesState["is_goto_fruit_ninja"] as bool? ?? false;
         }
 
-        bool IsGotoDiablo
-        {
-            get => story.variablesState["is_goto_diablo"] as bool? ?? false;
-        }
+
         void RenderStory()
         {
 
             var sceneManager = SceneManagerController.Instance.GetComponent<SceneManagerController>();
 
-            story.variablesState["is_fruit_ninja_done"] = IsFruitNinjaDone;
-            story.variablesState["is_diablo_done"] = IsDiabloDone;
-
             currentText = story.Continue();
-
-  
 
             if (IsGotoFruitNinja)
             {
@@ -61,16 +57,10 @@ namespace Assets.Scenes.Garden.Scripts
                 return;
             }
 
-            if (IsGotoDiablo)
-            {
-                sceneManager.GotoDiabloGameScene();
-                return;
-            }
-
             if (story.currentChoices.Count == 0)
-                {
-                    RenderStory();
-                }
+            {
+                RenderStory();
+            }
 
             RenderContent();
             RenderChoices();
@@ -81,15 +71,10 @@ namespace Assets.Scenes.Garden.Scripts
             contentLabel.text = currentText;
 
             contentContainer.RemoveFromClassList("god-content");
-            contentContainer.RemoveFromClassList("evil-content");
 
             if (CurrentSpeaker == Speaker.God)
             {
                 contentContainer.AddToClassList("god-content");
-            }
-            else if (CurrentSpeaker == Speaker.Boss)
-            {
-                contentContainer.AddToClassList("evil-content");
             }
         }
 
@@ -134,22 +119,22 @@ namespace Assets.Scenes.Garden.Scripts
             }
         }
 
-        bool IsFruitNinjaDone
-        {
-            get
-            {
-                var items = Inventory.instance.GetItems();
-                return items.Exists(x => x.name == "Fruit Memory");
-            }
-        }
-        bool IsDiabloDone
-        {
-            get
-            {
-                var items = Inventory.instance.GetItems();
-                return items.Exists(x => x.name == "Diablo Memory");
-            }
-        }
+        // bool IsFruitNinjaDone
+        // {
+        //     get
+        //     {
+        //         var items = Inventory.instance.GetItems();
+        //         return items.Exists(x => x.name == "Fruit Memory");
+        //     }
+        // }
+        // bool IsDiabloDone
+        // {
+        //     get
+        //     {
+        //         var items = Inventory.instance.GetItems();
+        //         return items.Exists(x => x.name == "Diablo Memory");
+        //     }
+        // }
 
         void OpenStory()
         {
